1ca614f9900613566a68377afb456475b7124fcd
Eduardo Piorini <eduardopiorini@hotmail.com>
2022-05-12 07:14:53 +0200

    authentication set

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
-import {NextApiRequest, NextApiResponse} from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (request:NextApiRequest, response: NextApiResponse) => {
+export default (request: NextApiRequest, response: NextApiResponse) => {
     const users = [
-        {id: 1, name: 'Eduardo'},
-        {id: 2, name: 'Melissa'},
-        {id: 3, name: 'Laura'},
+        { id: 1, name: 'Eduardo' },
+        { id: 2, name: 'Melissa' },
+        { id: 3, name: 'Laura' },
     ]
 
     return response.json(users)
