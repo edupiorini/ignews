e3cdce7cef0412421027c5c0dcb2c41c15c09bdc
Eduardo Piorini <eduardopiorini@hotmail.com>
2022-05-17 09:47:14 +0200

    stripe payment set

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,44 @@
-import styles from './styles.module.scss'
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe.js";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
-
+//3 lugares para utilizar secret keys
+//getServerSideProps (SSR)
+//getStaticProps (SSG)
+//API routes
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    return (
-        <button
-            className={styles.subscribeButton}
-            type="button"
-        >
-            Subscribe now
-        </button>
-    )
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+
+      return;
+      //criação da checkout session
+    }
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+  return (
+    <button
+      className={styles.subscribeButton}
+      type="button"
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
-import { GetStaticProps } from 'next'
+import { GetStaticProps } from "next";
 //import { GetServerSideProps } from 'next'
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
-import { stripe } from '../services/stripe';
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 //import Image from 'next/image';
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
 interface HomeProps {
   product: {
     priceId: string;
     amount: number;
-  }
+  };
 }
 export default function Home({ product }: HomeProps) {
   return (
@@ -23,7 +23,9 @@ export default function Home({ product }: HomeProps) {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome!</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
             Get access to all the publications <br />
             <span>for {product.amount}/month</span>
@@ -34,29 +36,26 @@ export default function Home({ product }: HomeProps) {
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
-
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1KxW4gGo9JMB2wHNVQgOA5nM')
+  const price = await stripe.prices.retrieve("price_1KxW4gGo9JMB2wHNVQgOA5nM");
 
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD'
-    }).format(price.unit_amount / 100)  //convertemos de centavos para  dolares
-  }
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100), //convertemos de centavos para  dolares
+  };
   return {
     props: {
       product,
     },
-    revalidate: 60 * 60 * 24 //24 hours
-
-  }
-
-}
+    revalidate: 60 * 60 * 24, //24 hours
+  };
+};
 
 /**
  * 
